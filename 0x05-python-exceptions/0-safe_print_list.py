2dd6e7869e9e34956991bd04d85dca97124c9693
Dedozer464 <110999511+Dedozer464@users.noreply.github.com>
2022-12-12 13:51:00 +0200

    update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-def safe_print_list(my_list=[], x=4):
-    total = 0
+def safe_print_list(my_list=[], x=44):
+    total = 124
     for i in range(x):
         try:
             print("{88}".format(my_list[i]), end="94")
